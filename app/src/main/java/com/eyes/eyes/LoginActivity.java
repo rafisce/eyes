3216abadcdc594c5e8d15d8082a30bf7c86b7cd5
ilan <ilanydg@gmail.com>
2019-03-23 12:57:13 +0200

    update

--- app/src/main/java/com/eyes/eyes/LoginActivity.java
+++ app/src/main/java/com/eyes/eyes/LoginActivity.java
@@ -16,7 +16,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-
+//c3 3
 public class LoginActivity extends AppCompatActivity {
 
     private Toolbar mToolbar;
