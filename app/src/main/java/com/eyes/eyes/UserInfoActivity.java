38cd3fc63e5834073a81b4df398e5062c3c88e47
SCEteam18 <34807159+SCEteam18@users.noreply.github.com>
2019-03-24 18:24:48 +0200

    updated project

---
+++ app/src/main/java/com/eyes/eyes/UserInfoActivity.java
@@ -0,0 +1,85 @@
+package com.eyes.eyes;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
+
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class UserInfoActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private DatabaseReference user;
+    private TextView user_name;
+    private  TextView user_email;
+    private  TextView lang;
+    private TextView created_date;
+    private Toolbar mToolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_info);
+
+        mToolbar = (Toolbar) findViewById(R.id.doc_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("דף ראשי");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        mAuth = FirebaseAuth.getInstance();
+        user = FirebaseDatabase.getInstance().getReference().child("Users").child(mAuth.getCurrentUser().getUid());
+
+        user_name = (TextView) findViewById(R.id.user_name_text_view);
+        user_email = (TextView) findViewById(R.id.user_email_text_view);
+        lang = (TextView) findViewById(R.id.lang_text_view);
+        created_date = (TextView) findViewById(R.id.created_date_text_view);
+
+        user.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                user_name.setText("user: "+dataSnapshot.child("user_name").getValue().toString());
+                user_email.setText("email: "+dataSnapshot.child("user_email").getValue().toString());
+                lang.setText("language: "+dataSnapshot.child("language").getValue().toString());
+                created_date.setText("joined in: "+dataSnapshot.child("create_date").getValue().toString());
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+
+
+    }
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        if(currentUser == null)
+        {
+            Intent startPageIntent = new Intent(UserInfoActivity.this,StartPageActivity.class);
+            startPageIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(startPageIntent);
+            finish();
+        }
+    }
+}
