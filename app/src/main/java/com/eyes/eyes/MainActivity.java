0fe444f84d272ca7b4e3e3413400f38f55fb46f8
shaked astemker <shaked2221@gmail.com>
2019-03-24 16:16:11 +0200

    change

--- app/src/main/java/com/eyes/eyes/MainActivity.java
+++ app/src/main/java/com/eyes/eyes/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(mToolbar);
         getSupportActionBar().setTitle("");
     }
-
+//12
     @Override
     protected void onStart()
     {
