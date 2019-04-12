74f73a08afd9d34f27733c4cdf6ac0ab00c5cb95
rafi <ruff6996@gmail.com>
2019-04-12 21:17:30 +0300

    "first"

--- app/src/main/java/com/eyes/eyes/MainActivity.java
+++ app/src/main/java/com/eyes/eyes/MainActivity.java
@@ -66,7 +66,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       
+
 
         mAuth = FirebaseAuth.getInstance();
 
